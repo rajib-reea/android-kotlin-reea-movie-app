2fd38ccd7e4377cd3fb73e3083e4d7d41673c549
rajib-reea <135492408+rajib-reea@users.noreply.github.com>
2023-06-07 15:28:03 +0600

    added MovieRemoteDataSource.

--- app/src/main/java/com/example/android_kotlin_reea_movie/data/MovieRemoteDataSource.kt
+++ app/src/main/java/com/example/android_kotlin_reea_movie/data/MovieRemoteDataSource.kt
@@ -1,4 +1,28 @@
 package com.example.android_kotlin_reea_movie.data;
 
-public class MovieRemoteDataSource {
+import retrofit2.Response
+import timber.log.Timber
+import java.lang.Exception
+
+internal sealed class MovieRemoteDataSource<T>(val data:  T?=null, val message:String?=null) {
+    class Loading<T>: MovieRemoteDataSource<T>()
+    class Success<T>(data: T?): MovieRemoteDataSource<T>(data=data)
+    class Failure<T>(data: T?, message: String?) : MovieRemoteDataSource<T>(data=data, message=message)
+
+    companion object{
+        suspend fun <T>  getMovies(fn: suspend ()->Response<T>): MovieRemoteDataSource<T>{
+             try {
+                val response=fn();
+                val body=response.body()
+                if (response.isSuccessful) {
+                    return Success<T>(data = body)
+                }
+            }catch (e: Exception){
+                Timber.e("Request Failed and the Error Message: $e")
+                return Failure<T>(data=null, message="Error happened.")
+            }
+
+        return TODO("Provide the return value")
+       }
+    }
 }
--- app/src/main/java/com/example/android_kotlin_reea_movie/data/MovieService.kt
+++ app/src/main/java/com/example/android_kotlin_reea_movie/data/MovieService.kt
@@ -1,4 +1,9 @@
 package com.example.android_kotlin_reea_movie.data;
+import com.example.android_kotlin_reea_movie.data.dto.MovieListDTO
+import com.skydoves.sandwich.ApiResponse
+import retrofit2.http.GET
 
-public class MovieService {
+internal interface MovieService {
+    @GET("")
+    suspend fun  getMovies(): ApiResponse<MovieListDTO>
 }
