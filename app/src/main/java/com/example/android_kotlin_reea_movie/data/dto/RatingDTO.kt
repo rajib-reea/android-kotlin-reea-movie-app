081c8219fa183fba5cb37e6a86bade1675cf9356
rajib-reea <135492408+rajib-reea@users.noreply.github.com>
2023-06-07 18:57:02 +0600

    .gitignore is changed.

--- app/src/main/java/com/example/android_kotlin_reea_movie/data/dto/RatingDTO.kt
+++ app/src/main/java/com/example/android_kotlin_reea_movie/data/dto/RatingDTO.kt
@@ -1,3 +1,8 @@
 package com.example.android_kotlin_reea_movie.data.dto
 
-internal class RatingDTO(val source:String, val value:String)
+import com.google.gson.annotations.SerializedName
+
+internal class RatingDTO(
+    @SerializedName("Source")  val source:String,
+    @SerializedName("Value")  val value:String
+    )
