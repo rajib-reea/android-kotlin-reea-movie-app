dcac0e2c6bbedfd27ad1e931f68bb753cb9a857d
rajib-reea <135492408+rajib-reea@users.noreply.github.com>
2023-06-14 18:34:24 +0600

    converting between entity and dto.

--- app/src/main/java/com/reeadigital/movieApp/data/datasource/local/MovieDatabase.kt
+++ app/src/main/java/com/reeadigital/movieApp/data/datasource/local/MovieDatabase.kt
@@ -1,21 +1,21 @@
 package com.reeadigital.movieApp.data.datasource.local
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
-import com.reeadigital.movieApp.MainApplication
 import com.reeadigital.movieApp.data.datasource.local.entity.Movie
 import com.reeadigital.movieApp.data.datasource.local.entity.MovieDetail
 import com.reeadigital.movieApp.data.datasource.local.entity.Rating
 
-@Database(entities = [Movie::class, MovieDetail::class, Rating::class], version = 1)
+@Database(entities = [Movie::class, MovieDetail::class, Rating::class], version = 1, exportSchema = true)
 abstract class MovieDatabase: RoomDatabase() {
     abstract fun movieDao(): MovieDao
     companion object{
         @Volatile
         private var INSTANCE: MovieDatabase? = null
 
-        fun getDatabase(context: MainApplication): MovieDatabase {
+        fun getDatabase(context: Context): MovieDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = databaseBuilder(
                     context,
--- app/src/main/java/com/reeadigital/movieApp/data/repository/MovieRepository.kt
+++ app/src/main/java/com/reeadigital/movieApp/data/repository/MovieRepository.kt
@@ -7,16 +7,14 @@ import com.reeadigital.movieApp.data.datasource.remote.movie.MovieApiService
 import com.reeadigital.movieApp.data.datasource.remote.movie.dto.MovieDTO
 import com.reeadigital.movieApp.data.datasource.remote.movie.dto.MovieDetailDTO
 import com.reeadigital.movieApp.data.datasource.remote.movie.dto.MovieListDTO
-import timber.log.Timber
 import java.time.LocalDateTime
-import java.time.Year
 import javax.inject.Inject
 
 class MovieRepository @Inject constructor(
     private val movieApi: MovieApiService
     ){
         private fun convertToEntity(movieDTO: MovieDTO): Movie {
-            val movie:Movie= Movie(
+            return Movie(
                 IMDB_ID = movieDTO.IMDB_ID,
                 Title = movieDTO.Title,
                 Type = movieDTO.Type,
@@ -26,7 +24,6 @@ class MovieRepository @Inject constructor(
                 created = LocalDateTime.now().toString(),
                 modified = LocalDateTime.now().toString()
             )
-            return movie
         }
         suspend fun getMovieDetail(detailID: String): MovieDetailDTO{
             //always get from the database
@@ -35,13 +32,14 @@ class MovieRepository @Inject constructor(
 
 
     suspend fun getMovieList(searchKey: String): MovieListDTO {
+        // val movieInstance=MovieDatabase.getDatabase(MainApplication())
         val movieList= movieApi.getMovieList(searchKey, "97edfc1b")
         val  movies=movieList.Search
 
         for (movieDTO in movies!!)  {
             //need to convert DTO object to ENTITY object
             val movie=convertToEntity(movieDTO)
-
+         //   movieInstance.movieDao().insertMovie(movie)
         }
         return movieList;
     }
